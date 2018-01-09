f70123180ebe458efebf06c3eb732eae9884ad3a
MehrabR <Mehrab.Rahman@Revature.com>
2018-01-09 15:51:52 -0500

    SpringXML

--- Week 6/SpringXMLDemo/src/main/java/com/revature/App.java
+++ Week 6/SpringXMLDemo/src/main/java/com/revature/App.java
@@ -1,13 +1,22 @@
 package com.revature;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import com.revature.repository.AssociateDAOImpl;
 import com.revature.service.PersonService;
 
 public class App {
 
 	static PersonService personService;
+
+	static AssociateDAOImpl associateDAO;
+	
+	@Autowired
+	public static void setAssociateDAO(AssociateDAOImpl associateDAO) {
+		App.associateDAO = associateDAO;
+	}
 
 	public static void main(String[] args) {
 
@@ -15,6 +24,10 @@ public class App {
 				new ClassPathXmlApplicationContext("applicationContext.xml");
 		personService = ac.getBean("personService", PersonService.class);
 		System.out.println(personService.getMe().getName());
+		
+		System.out.println(
+				ac.getBean("associateDAOImpl", AssociateDAOImpl.class)
+				.getPerson().getName());
 
 	}
 	

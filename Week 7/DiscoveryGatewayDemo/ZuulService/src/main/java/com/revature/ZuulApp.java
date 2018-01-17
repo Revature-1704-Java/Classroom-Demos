ac5bf994a4f0693e7ef2bc9df36e5b8012b793b9
MehrabR <Mehrab.Rahman@Revature.com>
2018-01-17 11:11:59 -0500

    ZuulEureka

---
+++ Week 7/DiscoveryGatewayDemo/ZuulService/src/main/java/com/revature/ZuulApp.java
@@ -0,0 +1,16 @@
+package com.revature;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+public class ZuulApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulApp.class, args);
+
+	}
+
+}
